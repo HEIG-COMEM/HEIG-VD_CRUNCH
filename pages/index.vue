2131e5aee183b9226d510b1db63fdbe14ce7780f
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-03-19 10:31:25 +0100

    style: apply styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <main class="w-sceen h-screen relative">
-    <div
-      class="absolute z-10 left-1/2 top-1/2 flex justify-center flex-col items-center"
-    >
-      <h1>Lorem</h1>
-      <Button>Button</Button>
-    </div>
-    <StunningGodRay />
-  </main>
+    <main class="w-sceen relative h-screen">
+        <div
+            class="absolute left-1/2 top-1/2 z-10 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center"
+        >
+            <BaseH1>EQUiD</BaseH1>
+            <Button>Button</Button>
+        </div>
+        <StunningGodRay />
+    </main>
 </template>
