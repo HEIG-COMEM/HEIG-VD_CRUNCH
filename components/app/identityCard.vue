6b301881d7772c656ca13364d8635ef5826fc2c5
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-03-20 10:35:44 +0100

    feat(loading): enhance loading state and UI for identification process feat(card): add identity card component for displaying horse details chore(dependencies): add @vueuse/core to dependencies

---
+++ components/app/identityCard.vue
@@ -0,0 +1,81 @@
+<script setup>
+const HORSE_NAME = 'Mustang'
+const HORSE_OWNER = 'Hugo'
+</script>
+
+<template>
+    <div class="flex flex-col gap-6">
+        <Card class="overflow-hidden">
+            <CardContent class="grid h-[80vh] p-0 md:grid-cols-2">
+                <div class="relative block bg-muted">
+                    <img
+                        src="https://thumbs.dreamstime.com/b/horse-head-5971041.jpg"
+                        alt="Image"
+                        class="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                    />
+                </div>
+                <form class="p-6 md:p-8">
+                    <div class="flex flex-col gap-6">
+                        <div class="flex flex-col items-center text-center">
+                            <h1 class="text-2xl font-bold">{{ HORSE_NAME }}</h1>
+                            <p class="text-balance text-muted-foreground">
+                                Owned by {{ HORSE_OWNER }}
+                            </p>
+                        </div>
+                        <div class="grid gap-2">
+                            <!-- FEI ID -->
+
+                            <Label for="feiId">FEI ID</Label>
+                            <Input
+                                id="feiId"
+                                disabled
+                                type="text"
+                                value="106EO39"
+                            />
+                        </div>
+                        <div class="grid gap-2">
+                            <!-- STUD BOOK -->
+                            <Label for="studBook">Stud Book</Label>
+                            <Input
+                                id="studBook"
+                                disabled
+                                type="text"
+                                value="KWPN"
+                            />
+                        </div>
+                        <div class="grid gap-2">
+                            <!-- REGISTRATION -->
+                            <Label for="registration">Registration</Label>
+                            <Input
+                                id="registration"
+                                disabled
+                                type="text"
+                                value="A18"
+                            />
+                        </div>
+                        <div class="grid gap-2">
+                            <!-- SEX -->
+                            <Label for="registration">Sex</Label>
+                            <Input
+                                id="registration"
+                                disabled
+                                type="text"
+                                value="Stallion"
+                            />
+                        </div>
+                        <div class="grid gap-2">
+                            <!-- DATE OF BIRTH -->
+                            <Label for="registration">Date of birth</Label>
+                            <Input
+                                id="registration"
+                                disabled
+                                type="date"
+                                value="2013-03-13"
+                            />
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+        </Card>
+    </div>
+</template>
--- pages/identify.vue
+++ pages/identify.vue
@@ -1,10 +1,10 @@
 <script setup>
-const isLoading = ref(false)
+const isLoading = ref(true)
 
 onMounted(() => {
     setTimeout(() => {
         isLoading.value = false
-    }, 5000)
+    }, 2500)
 })
 </script>
 
@@ -12,12 +12,17 @@ onMounted(() => {
     <main
         class="flex h-screen w-screen flex-col items-center justify-center gap-12 px-5 py-10"
     >
-        <div class="h-full w-full max-w-screen-sm bg-slate-200">
+        <div class="h-full w-full max-w-screen-sm">
             <template v-if="isLoading">
-                <div>loading...</div>
+                <div
+                    class="flex h-full w-full flex-col items-center justify-center"
+                >
+                    <AppLoading />
+                    <TypoLead>Loading...</TypoLead>
+                </div>
             </template>
             <template v-else>
-                <div>identify</div>
+                <AppIdentityCard />
             </template>
         </div>
     </main>
